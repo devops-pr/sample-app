6c8272430b67d5073c0e6a48a07c54783411ddfb
Piyush Raj <piyush.raj@outlook.in>
2020-08-10 11:57:55 +0530

    initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+# This is a sample Python script.
+
+# Press ⇧F10 to execute it or replace it with your code.
+# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "Welcome to the world of KUBER!"
+
+if __name__ == "__main__":
+    app.run()
